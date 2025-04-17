5c910e388ddc31d157e4ab7be7eb18940b36149f
shinys10 <166797095+sysys10@users.noreply.github.com>
2025-04-17 16:15:03 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Noto_Sans_KR } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const notosanskr = Noto_Sans_KR({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -18,8 +19,20 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <SEO />
       <body className={`${notosanskr.className} antialiased`}>{children}</body>
     </html>
+  )
+}
+
+function SEO() {
+  return (
+    <Head>
+      <title>DQV_HOME</title>
+      <meta name="description" content="DQV_HOME" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
   )
 }
