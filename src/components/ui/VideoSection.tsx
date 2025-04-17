6e4ea4ba94d0ddf274a351d8df6a360251a59e98
sttking <jiwonbond@gmail.com>
2025-04-17 16:13:35 +0900

    동영상 주소 변경

--- src/components/ui/VideoSection.tsx
+++ src/components/ui/VideoSection.tsx
@@ -7,7 +7,7 @@ interface YouTubeVideoProps {
   videoId: string
 }
 
-export default function VideoSection({ videoId = 'Mw_x4HYXHhI' }: YouTubeVideoProps) {
+export default function VideoSection({ videoId = 'amNjXnp-6-s' }: YouTubeVideoProps) {
   return (
     <motion.div className="w-full mt-8">
       <motion.div className="aspect-video w-full overflow-hidden rounded-lg shadow-lg">
